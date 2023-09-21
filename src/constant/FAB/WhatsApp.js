4d495fa324235359b3cac8ad4f7370d8f9751ce8
Sukhvant Singh <sukhvantsingh581998@gmail.com>
2023-09-22 00:26:04 +0530

    Initial commit

--- src/constant/FAB/WhatsApp.js
+++ src/constant/FAB/WhatsApp.js
@@ -4,7 +4,7 @@ import image from "../Images/image"
 const WhatsApp = () => {
   return (
     <>
-    <a className="fixed bottom-1/2 -right-10 py-2 px-3 rounded-lg bg-[#29A71A] text-white z-20 drop-shadow-lg font-semibold flex items-center gap-x-1 -rotate-90" href="https://wa.me/7906514988">
+    <a className="fixed bottom-1/2 -right-10 py-2 px-3 rounded-lg bg-[#29A71A] text-white z-20 drop-shadow-lg font-semibold flex items-center gap-x-1 -rotate-90" href="https://wa.me/1234567890">
      <Image src={image.whatsapp} height={25} width={25} alt="whats app"/>
      <p>What&apos;s App</p>
     </a>
