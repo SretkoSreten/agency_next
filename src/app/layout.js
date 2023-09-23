34915d5982e2310aa17e96f18b7eb0105a8814ff
Sukhvant Singh <sukhvantsingh581998@gmail.com>
2023-09-23 13:08:07 +0530

    Updated

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,6 +4,7 @@ import { Nunito} from 'next/font/google'
 import Header from '@/constant/Navbar/Header'
 import BottomBar from '@/constant/BottomBar/BottomBar'
 import WhatsApp from '@/constant/FAB/WhatsApp'
+import Head from 'next/head'
 
 const nunito = Nunito({ 
   weight: ['200','300','400','500','600','700','800'],
@@ -19,6 +20,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+    <Head>
+    <meta name="fl-verify" content="9be66eef7379e3946a735731ecf2c665"/>
+    </Head>
       <body className={nunito.className}>
       <Header/>
       {children}
